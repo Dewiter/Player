71f56a83f91eeb2ccda36114698ccd191177fd46
dewiter <herolezero@gmail.com>
2021-10-11 23:04:47 +0200

    added a react client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <section>
+      <form action=''>
+        <label htmlFor=''>send link</label>
+        <input type='text' />
+        <button>send</button>
+      </form>
+    </section>
+  );
+}
+
+export default App;
