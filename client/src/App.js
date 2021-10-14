07a7932f8c437138eb9bdacbe438f2933f98e39e
dewiter <herolezero@gmail.com>
2021-10-14 19:00:07 +0200

    second part : happy with design for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import Player from './components/player/Player';
+import Search from './components/player/Search';
+import Playlist from './components/player/Playlist';
 
 function App() {
   return (
     <>
-      <div className='big'>
-        <section className='container'>
+      <div className='app-container'>
+        <div className='media'>
+          <Search />
           <Player />
-        </section>
+        </div>
+        <div className='playlist'>
+          <Playlist />
+        </div>
       </div>
     </>
   );
