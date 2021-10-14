cb146335cd5a93b7b2a167039edf3c5e2b208243
dewiter <herolezero@gmail.com>
2021-10-14 19:24:32 +0200

    fixed modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,42 @@
+import React, { useReducer } from 'react';
 import Player from './components/player/Player';
 import Search from './components/player/Search';
 import Playlist from './components/player/Playlist';
+import Modal from './components/modal/Modal';
+
+import { queryController } from './controller/queryController';
 
 function App() {
+  const [state, dispatch] = useReducer(queryController, {
+    queue: [],
+    modalState: false,
+    modalContent: '',
+    modalType: '',
+  });
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' });
+  };
+
   return (
     <>
       <div className='app-container'>
         <div className='media'>
-          <Search />
-          <Player />
+          <Search handler={dispatch} state={state} />
+          <Player queue={state.queue} />
         </div>
         <div className='playlist'>
           <Playlist />
         </div>
       </div>
+
+      {state.modalState && (
+        <Modal
+          modalContent={state.modalContent}
+          modalType={state.modalType}
+          closeModal={closeModal}
+        />
+      )}
     </>
   );
 }
--- client/src/components/player/Player.js
+++ client/src/components/player/Player.js
@@ -1,33 +1,10 @@
-import React, { useReducer } from 'react';
-import Search from './Search';
 import Input from './Inputs/Inputs';
-import { queryController } from '../../controller/queryController';
-import Modal from '../modal/Modal';
 
-const Player = () => {
-  const [state, dispatch] = useReducer(queryController, {
-    queue: [],
-    modalState: false,
-    modalContent: '',
-    modalType: '',
-  });
-
-  const closeModal = () => {
-    dispatch({ type: 'CLOSE_MODAL' });
-  };
-
+const Player = ({ queue }) => {
   return (
     <div className='container container-out player'>
-      {/* <Search handler={dispatch} state={state} /> */}
       <h1>placeholder</h1>
-      <Input queue={state.queue} />
-      {state.modalState && (
-        <Modal
-          modalContent={state.modalContent}
-          modalType={state.modalType}
-          closeModal={closeModal}
-        />
-      )}
+      <Input queue={queue} />
     </div>
   );
 };
